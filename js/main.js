32412d6a8a36b193581b8db3befd33006389b807
jucimario <jucimarioo@live.com>
2023-02-17 12:29:24 -0300

    finalizados calculos estatisticos

---
+++ js/main.js
@@ -0,0 +1,77 @@
+const controle = document.querySelectorAll("[data-controle]");
+const estatisticas = document.querySelectorAll("[data-estatistica]");
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+controle.forEach((controle) => {
+    controle.addEventListener("click", (evento) => {
+        calcularEstatisticas(evento);
+        manipularDados(evento);
+    })
+})
+
+function manipularDados(eventoElement) {
+    const input = eventoElement.target.parentNode.querySelector("[data-contador]");
+    const operacao = eventoElement.target.dataset.controle;
+
+    if (operacao === "+") {
+        input.value = parseInt(input.value) + 1;
+    } else if (operacao === "-") {
+        if (parseInt(input.value) > 0) {
+            input.value = parseInt(input.value) - 1;
+        } 
+    }
+    else {
+        console.log("Operação inválida");
+    }
+}
+
+function calcularEstatisticas(peca) {
+
+    const item = peca.target.dataset.peca;
+    const operacao = peca.target.dataset.controle;
+    const input = peca.target.parentNode.querySelector("[data-contador]");
+
+    estatisticas.forEach((elemento,) => {
+        if (operacao === "+") {
+            elemento.textContent = parseInt(elemento.textContent) + parseInt(pecas[item][elemento.dataset.estatistica]);
+        } else if (operacao === "-") {
+            if (parseInt(input.value) > 0) {
+                elemento.textContent = parseInt(elemento.textContent) - parseInt(pecas[item][elemento.dataset.estatistica]);
+            } else {
+                console.log("Não há mais " + item + " para serem removidos");
+            }
+        }
+    })
+}
